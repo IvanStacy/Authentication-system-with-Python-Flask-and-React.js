ba70f51e96a92926afab610605850ae79e6816f8
IvanStacy <139801928+IvanStacy@users.noreply.github.com>
2024-01-09 18:58:32 +0000

    Done

--- src/front/js/pages/private.js
+++ src/front/js/pages/private.js
@@ -2,18 +2,18 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export const Private = () => {
-    const [isValid, setIsValid] = useState(null)
+    const [isValid, setIsValid] = useState("pending")
     const navigate = useNavigate()
     useEffect(() => {
         const checkToken = async () => {
             try {
                 let response = await fetch(process.env.BACKEND_URL + "/api/private",
                     { headers: { Authorization: "Bearer " + sessionStorage.getItem("token") } })
-                if (response.status == 200){
+                if (response.status == 200) {
                     setIsValid(true)
                 }
                 else {
-                    isValid(false)
+                    setIsValid(false)
                 }
 
             }
@@ -23,13 +23,15 @@ export const Private = () => {
         }
         checkToken()
     }, []);
-    switch (isValid){
-        case true: 
-        return (
-            <div>Private</div>
-        )
+    switch (isValid) {
+        case "pending":
+            return ("checking your access status")
+        case true:
+            return (
+                <div>Congrats you have access!</div>
+            )
         case false:
-        return("You don't have access")
+            return ("Sorry, you don't have access")
     }
-    
+
 }
